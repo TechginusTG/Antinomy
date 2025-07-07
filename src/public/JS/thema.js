cdd89720e7153b14ad69df03237b3fc1b70d685d
system0105 <songsy200701@gmail.com>
2025-07-07 23:19:49 +0900

    themaJS

--- src/public/JS/thema.js
+++ src/public/JS/thema.js
@@ -5,5 +5,25 @@ const thema = {
         btn: '#ffffff',
         text: '#9c5a65'
     },
-    natsu:
+
+    natsu:{
+        bg: '#f3e9d2',
+        chat: '#2a6f9e',
+        btn: '#e7d9b9',
+        text: '#2a6f9e'
+    },
+
+    aki:{
+        bg: '#f7f1df',
+        chat: '#d2b48c',
+        btn: '#DEB887',
+        text: '#f7f1df'
+    },
+
+    fuyu:{
+        bg: '#F8F8FF',
+        chat: '#8B0000',
+        btn: '#006400',
+        text: '#FFFFFF'
+    }
 }
