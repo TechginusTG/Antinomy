1da12acf7588c3b876ee9573106b017600a587d4
naya1228 <jin100y43@gmail.com>
2025-07-07 23:04:02 +0900

    edit example

--- src/public/JS/thema.js
+++ src/public/JS/thema.js
@@ -1,9 +1,9 @@
 const thema = {
     haru:{
-        bg-color: '#fff3f5',
-        chat-color: '#f9d5d3',
-        btn-color: '#ffffff',
-        text-color: '#9c5a65'
+        bg: '#fff3f5',
+        chat: '#f9d5d3',
+        btn: '#ffffff',
+        text: '#9c5a65'
     },
     natsu:
 }
