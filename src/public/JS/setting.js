517889212ffbf40b654a2d1c581ed359b7eb6dc4
naya1228 <jin100y43@gmail.com>
2025-07-08 09:37:59 +0900

    a

--- src/public/JS/setting.js
+++ src/public/JS/setting.js
@@ -87,11 +87,6 @@ function applySettings() {
 // 초기 라벨 설정
 updateLabels(document.getElementById("language").value);
 
-// ...existing code...
-
-// 초기 라벨 설정
-updateLabels(document.getElementById("language").value);
-
 // 현재 선택된 테마를 radio에 반영
 const savedTheme = window.localStorage.getItem("thema") || "light";
 document.querySelectorAll('input[name="theme"]').forEach((radio) => {
