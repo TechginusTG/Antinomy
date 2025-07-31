5b0363aa6df691e715d384d67e8c6198872c6f20
gganwoor <gganwoor@gmail.com>
2025-08-01 05:55:23 +0900

    테마별 전역변수 추가
    개노가다를 더노가다로 정상화시킨 나
    이후 다른 테마 추가 및 관리를 위해 파일 하나 만들어서 전역변수 추가함
    자잘한 수정

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LandingPage from './pages/Landing/Landing.jsx';
 import MainApp from './pages/MainApp/MainApp.jsx';
+import './index.css'
 
 const router = createBrowserRouter([
   {
