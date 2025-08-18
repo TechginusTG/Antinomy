f8b40bf594fd79e434e9f8077f1d403173259e96
naya1228 <jin100y43@gmail.com>
2025-08-18 18:41:59 +0900

    헤더가 다시 반응합니다.

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Layout } from 'antd';
-import ConnectionStatus from '../ConnectionStatus/ConnectionStatus';
-import styles from './HeaderBar.module.css';
+import React from "react";
+import { Layout, Button } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import ConnectionStatus from "../ConnectionStatus/ConnectionStatus";
+import styles from "./HeaderBar.module.css";
 
 const HeaderBar = ({ className }) => {
   return (
     <Layout.Header className={`${styles.header} ${className}`}>
+      <Button
+        className={styles.menuButton}
+        icon={<MenuOutlined />}
+        onClick={toggleSider}
+      />
       <div className={styles.titleContainer}>
         <h2>ANTINOMY</h2>
         <ConnectionStatus />
