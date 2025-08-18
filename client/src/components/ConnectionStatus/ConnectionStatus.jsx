d1eec548bdfeced00d7c29a32009c3e98493fdc8
gganwoor <139461535+gganwoor@users.noreply.github.com>
2025-08-18 16:21:41 +0900

    상태표시등이 뭔지 모르는 몬난놈들을 위한 첫번째 레슨
    마우스 올려보기

--- client/src/components/ConnectionStatus/ConnectionStatus.jsx
+++ client/src/components/ConnectionStatus/ConnectionStatus.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Tooltip } from 'antd';
 import useFlowStore from '../../utils/flowStore';
 import styles from './ConnectionStatus.module.css';
 
 const ConnectionStatus = () => {
   const isConnected = useFlowStore((state) => state.isConnected);
+  const tooltipText = isConnected ? '서버에 연결됨' : '서버에 연결되지 않음';
 
   return (
-    <div 
-      className={`${styles.statusIndicator} ${isConnected ? styles.connected : styles.disconnected}`}
-      title={isConnected ? 'Connected' : 'Disconnected'}
-    />
+    <Tooltip title={tooltipText}>
+      <div 
+        className={`${styles.statusIndicator} ${isConnected ? styles.connected : styles.disconnected}`}
+      />
+    </Tooltip>
   );
 };
 
