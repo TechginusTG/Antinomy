f166539ebcc1ff22ba699877e82b7c3995b735ca
gganwoor <139461535+gganwoor@users.noreply.github.com>
2025-08-19 09:30:31 +0900

    빛 없는 그림 안=티노미 로 고
    다크 모드에서 로고 색상이 반전되도록 적용(다크 테마에서만)

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -4,8 +4,11 @@ import { MenuOutlined } from "@ant-design/icons";
 import ConnectionStatus from "../ConnectionStatus/ConnectionStatus";
 import styles from "./HeaderBar.module.css";
 import logo from "../../assets/img/logo.png"; // Adjust the path as necessary
+import useFlowStore from "../../utils/flowStore";
 
 const HeaderBar = ({ className, toggleSider }) => {
+  const theme = useFlowStore((state) => state.theme);
+
   return (
     <Layout.Header className={`${styles.header} ${className}`}>
       <Button
@@ -14,7 +17,11 @@ const HeaderBar = ({ className, toggleSider }) => {
         onClick={toggleSider}
       />
       <div className={styles.titleContainer}>
-        <img src={logo} alt="Antinomy Logo" className={styles.logo} />
+        <img 
+          src={logo} 
+          alt="Antinomy Logo" 
+          className={`${styles.logo} ${theme === 'dark' ? styles.darkThemeLogo : ''}`} 
+        />
         <h2 className={styles.title}>ANTINOMY</h2>
         <ConnectionStatus />
       </div>
