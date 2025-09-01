f3ea88eabf4c3a948a9d3b664f27b654e91535ac
gganwoor <gganwoor@gmail.com>
2025-09-01 18:49:47 +0900

    개시ㅏㅂㄹ

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './server/dev.sqlite3'
-    },
-    useNullAsDefault: true,
+    client: 'pg',
+    connection: 'postgresql://postgres:[s5iw1WQZXehzruyp]@[2406:da12:b78:de01:ef3a:88e3:ae16:c76]:5432/postgres',
     migrations: {
       directory: './migrations'
     },
