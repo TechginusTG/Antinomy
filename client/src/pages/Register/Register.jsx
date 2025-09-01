2cd0d311adc3fd444a3f1c9d916a80d8bb6165dc
gganwoor <gganwoor@gmail.com>
2025-09-01 21:15:46 +0900

    내가이겼다내가이겼다고하루종일장장13시간동안의데이터베이스와의사투가오후9시15분을기점으로종료되었음을선포한다게마이니수고했어

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -12,7 +12,7 @@ const Register = ({ onRegistrationSuccess, switchToLogin }) => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch('/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- knexfile.cjs
+++ knexfile.cjs
@@ -1,12 +1,30 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgresql://postgres:[s5iw1WQZXehzruyp]@[2406:da12:b78:de01:ef3a:88e3:ae16:c76]:5432/postgres',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './migrations'
     },
     seeds: {
       directory: './seeds'
+    },
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    ssl: {
+      rejectUnauthorized: false
     }
   }
 };
