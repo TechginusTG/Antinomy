0d056bb176abdedfd9d14f2b2988988ae8c5e326
naya1228 <jin100y43@gmail.com>
2025-09-08 13:38:29 +0900

    명언

--- client/src/components/DiagramMessage/DiagramMessage.jsx
+++ client/src/components/DiagramMessage/DiagramMessage.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './DiagramMessage.module.css';
+import { quotes } from '../../utils/quotes/quotes.js';
 
 const DiagramMessage = ({ message }) => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    if (message) {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      setQuote(quotes[randomIndex]);
+    }
+  }, [message]);
+
   if (!message) {
     return null;
   }
 
   return (
     <div className={styles.overlay}>
-      <div className={styles.message}>{message}</div>
+      <div>
+        <div className={`${styles.message} ${styles.blinkingText}`}>{message}</div>
+        {quote && <div className={styles.quoteText}>{quote}</div>}
+      </div>
     </div>
   );
 };
